203da4bc34ea0f4e75aa26cb7ec3084edd1444c2
yankarsten <yanfkarsten@gmail.com>
2024-02-26 23:51:09 -0300

    First Commit

---
+++ backend/index.js
@@ -0,0 +1,104 @@
+import express, {
+    response
+} from "express";
+import {
+    PORT,
+    mongoDBURL
+} from "./config.js";
+import mongoose from 'mongoose';
+import {
+    User
+} from "./models/userModel.js";
+
+const app = express();
+
+app.use(express.json());
+
+//Rota para adicionar um usuário
+app.post('/users', async (request, response) => {
+    try {
+
+        const newUser = {
+            imagem: request.body.imagem,
+            nome: request.body.nome,
+            cpf: request.body.cpf,
+            telefone: request.body.telefone,
+            nascimento: request.body.nascimento,
+        };
+
+        const user = await User.create(newUser);
+
+        return response.status(201).send(user);
+
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({
+            message: error.message
+        });
+    }
+});
+
+//Rota para obter todos os usuários
+app.get('/users', async (request, response) => {
+    try {
+        const users = await User.find({});
+
+        return response.status(200).json({
+            count: users.length,
+            data: users
+        })
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({
+            message: error.message
+        });
+    }
+});
+
+//Rota para obter usuário por id
+app.get('/users/:id', async (request, response) => {
+    try {
+
+        const {
+            id
+        } = request.params;
+        const user = await User.findById(id);
+        return response.status(200).json(user)
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({
+            message: error.message
+        });
+    }
+});
+
+//Rota para editar um usuário
+app.put('/users/:id', async (request, response) => {
+    try {
+        const { id } = request.params;
+    
+        const result = await User.findByIdAndUpdate(id, request.body);
+    
+        if (!result) {
+          return response.status(404).json({ message: 'Usuário não encontrado' });
+        }
+    
+        return response.status(200).send({ message: 'Usuário atualizado com sucesso' });
+      } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+      }
+    });
+
+
+mongoose
+    .connect(mongoDBURL)
+    .then(() => {
+        console.log('Conectado ao banco de dados');
+        app.listen(PORT, () => {
+            console.log(`Ouvindo a porta: ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.log(error)
+    });
---
+++ backend/models/userModel.js
@@ -0,0 +1,31 @@
+import mongoose from 'mongoose';
+
+var userSchema = mongoose.Schema(
+  {
+    imagem: {
+      type: String,
+      required: true,
+    },
+    nome: {
+      type: String,
+      required: true,
+    },
+    cpf: {
+      type: String,
+      required: true,
+    },
+    telefone: {
+        type: String,
+        required: true,
+    },
+    nascimento: {
+        type: String,
+        required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const User = mongoose.model('User', userSchema);
