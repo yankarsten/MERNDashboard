c074d01dc45f9c8a39290740683d8c638afe1785
yankarsten <yanfkarsten@gmail.com>
2024-02-27 01:34:53 -0300

    Refatoração das Rotas

--- backend/index.js
+++ backend/index.js
@@ -1,95 +1,23 @@
-import express, {
-    response
-} from "express";
-import {
-    PORT,
-    mongoDBURL
-} from "./config.js";
+import express, {response} from "express";
+import {PORT,mongoDBURL} from "./config.js";
 import mongoose from 'mongoose';
-import {
-    User
-} from "./models/userModel.js";
+import {User} from "./models/userModel.js";
+import usersRoute from './routes/usersRoute.js';
 
 const app = express();
 
+//Middleware para a requisição do corpo da request
 app.use(express.json());
 
-//Rota para adicionar um usuário
-app.post('/users', async (request, response) => {
-    try {
+//Middleware para o manuseio de CORS
+app.use(cors());
 
-        const newUser = {
-            imagem: request.body.imagem,
-            nome: request.body.nome,
-            cpf: request.body.cpf,
-            telefone: request.body.telefone,
-            nascimento: request.body.nascimento,
-        };
-
-        const user = await User.create(newUser);
-
-        return response.status(201).send(user);
-
-    } catch (error) {
-        console.log(error.message);
-        response.status(500).send({
-            message: error.message
-        });
-    }
+app.get('/', (request, response) => {
+    console.log(request);
+    return response.status(234).send('Dashboard de cadastro de Usuários');
 });
 
-//Rota para obter todos os usuários
-app.get('/users', async (request, response) => {
-    try {
-        const users = await User.find({});
-
-        return response.status(200).json({
-            count: users.length,
-            data: users
-        })
-    } catch (error) {
-        console.log(error.message);
-        response.status(500).send({
-            message: error.message
-        });
-    }
-});
-
-//Rota para obter usuário por id
-app.get('/users/:id', async (request, response) => {
-    try {
-
-        const {
-            id
-        } = request.params;
-        const user = await User.findById(id);
-        return response.status(200).json(user)
-    } catch (error) {
-        console.log(error.message);
-        response.status(500).send({
-            message: error.message
-        });
-    }
-});
-
-//Rota para editar um usuário
-app.put('/users/:id', async (request, response) => {
-    try {
-        const { id } = request.params;
-    
-        const result = await User.findByIdAndUpdate(id, request.body);
-    
-        if (!result) {
-          return response.status(404).json({ message: 'Usuário não encontrado' });
-        }
-    
-        return response.status(200).send({ message: 'Usuário atualizado com sucesso' });
-      } catch (error) {
-        console.log(error.message);
-        response.status(500).send({ message: error.message });
-      }
-    });
-
+app.use('/users', usersRoute);
 
 mongoose
     .connect(mongoDBURL)
---
+++ backend/routes/usersRoute.js
@@ -0,0 +1,83 @@
+import express from 'express';
+import {User} from '../models/userModel.js';
+
+const router = express.Router();
+
+
+//Rota para obter todos os usuários
+router.get('/', async (request, response) => {
+    try {
+        const users = await User.find({});
+
+        return response.status(200).json({
+            count: users.length,
+            data: users
+        })
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({
+            message: error.message
+        });
+    }
+});
+
+//Rota para obter usuário por id
+router.get('/:id', async (request, response) => {
+    try {
+
+        const {
+            id
+        } = request.params;
+        const user = await User.findById(id);
+        return response.status(200).json(user)
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({
+            message: error.message
+        });
+    }
+});
+
+//Rota para adicionar um usuário
+router.post('/', async (request, response) => {
+    try {
+
+        const newUser = {
+            imagem: request.body.imagem,
+            nome: request.body.nome,
+            cpf: request.body.cpf,
+            telefone: request.body.telefone,
+            nascimento: request.body.nascimento,
+        };
+
+        const user = await User.create(newUser);
+
+        return response.status(201).send(user);
+
+    } catch (error) {
+        console.log(error.message);
+        response.status(500).send({
+            message: error.message
+        });
+    }
+});
+
+//Rota para deletar um usuário
+router.delete('/:id', async (request, response) => {
+    try {
+        const { id } = request.params;
+    
+        const result = await User.findByIdAndDelete(id);
+    
+        if (!result) {
+          return response.status(404).json({ message: 'Usuário não encontrado' });
+        }
+    
+        return response.status(200).send({ message: 'Usuário deletado com sucesso' });
+      } catch (error) {
+        console.log(error.message);
+        response.status(500).send({ message: error.message });
+      }
+    });
+
+export default router;
